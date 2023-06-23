224569f5820fadfbdb47c38c46f4b31e046e97f4
xcy <mail@xiechaoyi.com>
2023-06-23 11:04:10 +0800

    update

--- Wlniao.XCore/Extend/DbConnectInfo.cs
+++ Wlniao.XCore/Extend/DbConnectInfo.cs
@@ -12,31 +12,61 @@ namespace Wlniao
     {
         private static string connstr_rw = null;  //读写链接
         private static string connstr_ro = null;  //只读链接
-        private static string connstr_mysql = null;
-        private static string connstr_sqlite = null;
+		private static string connstr_type = null;
+		private static string connstr_mysql = null;
+		private static string connstr_sqlite = null;
         private static string connstr_sqlsqlver = null;
-        /// <summary>
-        /// 数据库连接字符串
-        /// </summary>
-        public static string WLN_CONNSTR
-        {
+		/// <summary>
+		/// 数据库连接字符串
+		/// </summary>
+		public static string WLN_CONNSTR
+		{
+			get
+			{
+				if (connstr_rw == null)
+				{
+					connstr_rw = Wlniao.Config.GetConfigs("WLN_CONNSTR");
+				}
+				if (string.IsNullOrEmpty(connstr_rw))
+				{
+					connstr_rw = WLN_CONNSTR_MYSQL;
+				}
+				return connstr_rw;
+			}
+		}
+		/// <summary>
+		/// 数据库连接类型 默认：sqlite/mysql
+		/// </summary>
+		public static string WLN_CONNSTR_TYPE
+		{
             get
             {
-                if (connstr_rw == null)
+                if (string.IsNullOrEmpty(connstr_type))
                 {
-                    connstr_rw = Wlniao.Config.GetConfigs("WLN_CONNSTR");
+                    connstr_type = Wlniao.Config.GetConfigs("WLN_CONNSTR_TYPE");
+                    if (string.IsNullOrEmpty(connstr_type))
+                    {
+                        if (!string.IsNullOrEmpty(WLN_CONNSTR_MYSQL))
+                        {
+                            connstr_type = "mysql";
+                        }
+                        else if (!string.IsNullOrEmpty(WLN_CONNSTR_SQLSERVER))
+                        {
+                            connstr_type = "sqlserver";
+                        }
+                        else
+                        {
+                            connstr_type = "sqlite";
+                        }
+                    }
                 }
-                if (string.IsNullOrEmpty(connstr_rw))
-                {
-                    connstr_rw = WLN_CONNSTR_MYSQL;
-                }
-                return connstr_rw;
+                return connstr_type;
             }
-        }
-        /// <summary>
-        /// 连接的数据库服务器地址（默认为127.0.0.1）
-        /// </summary>
-        public static string WLN_CONNSTR_HOST
+		}
+		/// <summary>
+		/// 连接的数据库服务器地址（默认为127.0.0.1）
+		/// </summary>
+		public static string WLN_CONNSTR_HOST
         {
             get
             {
@@ -108,22 +138,44 @@ namespace Wlniao
                         var WLN_MYSQL_NAME = Wlniao.Config.GetConfigs("WLN_MYSQL_NAME", WLN_CONNSTR_NAME);
                         var WLN_MYSQL_UID = Wlniao.Config.GetEncrypt("WLN_MYSQL_UID", Config.Secret, WLN_CONNSTR_UID);
                         var WLN_MYSQL_PWD = Wlniao.Config.GetEncrypt("WLN_MYSQL_PWD", Config.Secret, WLN_CONNSTR_PWD);
-                        connstr_mysql = string.Format("Server={0};Port={1};Database={2};Uid={3};Pwd={4};CharSet=utf8;SslMode=none;"
-                            , WLN_MYSQL_HOST, WLN_MYSQL_PORT, WLN_MYSQL_NAME, WLN_MYSQL_UID, WLN_MYSQL_PWD);
-                        if (string.IsNullOrEmpty(WLN_MYSQL_HOST) || string.IsNullOrEmpty(WLN_MYSQL_UID) || string.IsNullOrEmpty(WLN_MYSQL_PWD))
-                        {
-                            connstr_mysql = "";
-                        }
+                        if (!string.IsNullOrEmpty(WLN_MYSQL_HOST) && !string.IsNullOrEmpty(WLN_MYSQL_UID) && !string.IsNullOrEmpty(WLN_MYSQL_PWD))
+						{
+							connstr_mysql = string.Format("Server={0};Port={1};Database={2};Uid={3};Pwd={4};CharSet=utf8;SslMode=none;"
+								, WLN_MYSQL_HOST, WLN_MYSQL_PORT, WLN_MYSQL_NAME, WLN_MYSQL_UID, WLN_MYSQL_PWD);
+						}
                     }
                 }
                 return connstr_mysql;
             }
         }
 
-        /// <summary>
-        /// SqlServer数据库连接字符串
-        /// </summary>
-        public static string WLN_CONNSTR_SQLSERVER
+		/// <summary>
+		/// Sqlite数据库连接字符串
+		/// </summary>
+		public static string WLN_CONNSTR_SQLITE
+		{
+			get
+			{
+				if (connstr_sqlite == null)
+				{
+					connstr_sqlite = Wlniao.Config.GetSetting("WLN_CONNSTR_SQLITE");
+					if (string.IsNullOrEmpty(connstr_sqlite) && string.IsNullOrEmpty(WLN_CONNSTR_NAME))
+					{
+						var sqlite = Wlniao.IO.PathTool.Map(Wlniao.XCore.StartupRoot, XCore.FrameworkRoot, "xcore.db");
+						if (IO.FileEx.Exists(sqlite))
+						{
+							connstr_sqlite = "Data Source=" + sqlite;
+						}
+					}
+				}
+				return connstr_sqlite;
+			}
+		}
+
+		/// <summary>
+		/// SqlServer数据库连接字符串
+		/// </summary>
+		public static string WLN_CONNSTR_SQLSERVER
         {
             get
             {
@@ -132,43 +184,21 @@ namespace Wlniao
                     connstr_sqlsqlver = Wlniao.Config.GetConfigs("WLN_CONNSTR_SQLSERVER");
                     if (string.IsNullOrEmpty(connstr_sqlsqlver))
                     {
-                        var WLN_MSSQL_PORT = Wlniao.Config.GetConfigs("WLN_MSSQL_PORT", "1433");
                         var WLN_MSSQL_HOST = Wlniao.Config.GetConfigs("WLN_MSSQL_HOST", WLN_CONNSTR_HOST);
                         var WLN_MSSQL_NAME = Wlniao.Config.GetConfigs("WLN_MSSQL_NAME", WLN_CONNSTR_NAME);
                         var WLN_MSSQL_UID = Wlniao.Config.GetEncrypt("WLN_MSSQL_UID", Config.Secret, WLN_CONNSTR_UID);
                         var WLN_MSSQL_PWD = Wlniao.Config.GetEncrypt("WLN_MSSQL_PWD", Config.Secret, WLN_CONNSTR_PWD);
-                        connstr_sqlsqlver = string.Format("Server={0},{1};Database={2};User Id={3};Password={4};TrustServerCertificate=true;"
-                            , WLN_MSSQL_HOST, WLN_MSSQL_PORT, WLN_MSSQL_NAME, WLN_MSSQL_UID, WLN_MSSQL_PWD);
-                        if (string.IsNullOrEmpty(WLN_MSSQL_NAME) || string.IsNullOrEmpty(WLN_MSSQL_UID) || string.IsNullOrEmpty(WLN_MSSQL_PWD))
-                        {
-                            connstr_sqlsqlver = "";
-                        }
+                        if (!string.IsNullOrEmpty(WLN_MSSQL_NAME) && !string.IsNullOrEmpty(WLN_MSSQL_UID) && !string.IsNullOrEmpty(WLN_MSSQL_PWD))
+						{
+							var WLN_MSSQL_PORT = Wlniao.Config.GetConfigs("WLN_MSSQL_PORT", "1433");
+							connstr_sqlsqlver = string.Format("Server={0},{1};Database={2};User Id={3};Password={4};TrustServerCertificate=true;"
+								, WLN_MSSQL_HOST, WLN_MSSQL_PORT, WLN_MSSQL_NAME, WLN_MSSQL_UID, WLN_MSSQL_PWD);
+						}
                     }
                 }
                 return connstr_sqlsqlver;
             }
         }
-        /// <summary>
-        /// Sqlite数据库连接字符串
-        /// </summary>
-        public static string WLN_CONNSTR_SQLITE
-        {
-            get
-            {
-                if (connstr_sqlite == null)
-                {
-                    connstr_sqlite = Wlniao.Config.GetSetting("WLN_CONNSTR_SQLITE");
-                    if (string.IsNullOrEmpty(connstr_sqlite) && string.IsNullOrEmpty(WLN_CONNSTR_NAME))
-                    {
-                        var sqlite = Wlniao.IO.PathTool.Map(Wlniao.XCore.StartupRoot, XCore.FrameworkRoot, "xcore.db");
-                        if (IO.FileEx.Exists(sqlite))
-                        {
-                            connstr_sqlite = "Data Source=" + sqlite;
-                        }
-                    }
-                }
-                return connstr_sqlite;
-            }
-        }
+
     }
 }
